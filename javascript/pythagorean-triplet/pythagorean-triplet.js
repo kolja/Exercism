06bea4f65618559913b07161506ccaf74a0c0c53
Kolja Wilcke <kolja@apfel.de>
2023-06-22 14:16:13 +0200

    added javascript

---
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -0,0 +1,13 @@
+export class Triplet {
+
+    constructor([a, b, c]) {}
+
+    sum() {
+        return this.a + this.b + this.c
+    }
+
+    product() {
+        return this.a * this.b * this.c
+}
+
+

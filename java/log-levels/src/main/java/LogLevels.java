3c65c917d16e5706676bacf3e3b45a8b1cc1c47c
Kolja Wilcke <kolja@apfel.de>
2023-06-25 21:25:32 +0200

    java: LogLevels

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,16 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LogLevels {
 
-    static final String PATTERN = "\\[([INFO|WARNING|ERROR])\\]:\s(.+)\s$";
+    static final Pattern PATTERN = Pattern.compile("\\[(INFO|WARNING|ERROR)\\]:\\s*(.+)");
+
+    static String[] match( String input ) {
+        String[] result = new String[2];
+        Matcher matcher = PATTERN.matcher(input);
+        if (matcher.find()) {
+            result[0] = matcher.group(1).toLowerCase();
+            result[1] = matcher.group(2).trim();
+        }
+        return result;
+    }
 
     public static String message(String logLine) {
-        return logLine.replaceAll( PATTERN, "$2" );
+        return match(logLine)[1];
     }
 
     public static String logLevel(String logLine) {
-        return logLine.replaceAll( PATTERN, "$1" );
+        return match(logLine)[0];
     }
 
     public static String reformat(String logLine) {
-        return logLine.replaceAll( PATTERN, "($2) $1" );
+        String[] result = match(logLine);
+        return String.format("%s (%s)", result[1], result[0]);
     }
 }
